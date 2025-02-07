f72e0c0236ae6fb093a6215e12269de9f9b37d31
Pham-Ngoc-Huy <huypham10122001@gmail.com>
2025-02-07 20:22:29 +0700

    Update HackerRank String

--- LabReview/HackerRank_String.cpp
+++ LabReview/HackerRank_String.cpp
@@ -7,12 +7,13 @@ int main(){
     vector<string>numbers;
     string read;
     string num;
+    //char type: using in for get an element in string
     getline(cin,read);
     stringstream ss(read);
-    while (getline(ss,num, ',')){
-        numbers.push_back(num);
+    while (getline(ss,num, ',')){ // get each element in an string "23,55,45" - seperate by ','
+        numbers.push_back(num); //adding back to the vector
     }
-    for (const string& i:numbers){
+    for (const string& i:numbers){ // loop for each element in vector
         cout<<i<<endl;
     }
     return 0;
