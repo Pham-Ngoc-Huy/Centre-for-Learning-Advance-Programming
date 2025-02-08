9c7e742b55b5ecb8b829de41967f57809c01bffe
Pham-Ngoc-Huy <huypham10122001@gmail.com>
2025-02-08 13:29:15 +0700

    Update Salary

---
+++ LabReview/Employee_ChatGPT_Exe/library.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "library.h"
+
+using namespace std;
+
+double FullTimeEmployee::calculateSalary() const {
+    return monthlysalary;
+}
+void FullTimeEmployee::display() const{
+    cout << "Full-Time Employee: "<<name <<" ("<<"ID: "
+    <<id<<" )"<<"\nSalary: "<<"$"<<calculateSalary()<<endl;
+}
+double PartTimeEmployee::calculateSalary() const {
+    return hourlywage * hoursworked;
+}
+void PartTimeEmployee::display() const{
+    cout << "Hours Worked: "<<hoursworked<<", "<<"Hourly Wage:"<<"$"<<hourlywage
+    <<"\nSalary: "<<"$"<<calculateSalary()<<endl;
+}
+double Manager::calculateSalary() const {
+    return FullTimeEmployee::calculateSalary() + PartTimeEmployee::calculateSalary();
+}   
+void Manager::display() const{
+    cout <<"Manager: "<<FullTimeEmployee::name<<" (ID: "<<FullTimeEmployee::id<<")"
+    <<"\nBase Salary: "<<"$"<<FullTimeEmployee::calculateSalary()
+    <<", Bonus: "<<"$"<<PartTimeEmployee::calculateSalary()
+    <<"\nTotal Salary: "<<"$"<<calculateSalary()<<endl;
+}
+
+
---
+++ LabReview/Employee_ChatGPT_Exe/library.h
@@ -0,0 +1,46 @@
+#ifndef _DEFINE_EMPLOYEE_
+#define _DEFINE_EMPLOYEE_
+#include <iostream>
+using namespace std;
+
+class Employee{
+    protected:
+        string name;
+        int id;
+    public:
+        //constructor
+        Employee(): name(" "), id(0) {};
+        Employee(const string& name_, const int& id_): name(name_), id(id_) {};
+        virtual double calculateSalary() const = 0;
+        virtual void display() const = 0;
+        //destructor
+        virtual ~Employee(){};
+};
+class FullTimeEmployee : public Employee{
+    protected:
+        int monthlysalary;
+    public:
+        FullTimeEmployee(const string& name_, const int& id_, const int& salary_): Employee(name_, id_), monthlysalary(salary_){};
+        double calculateSalary() const override;
+        void display() const override;
+};
+class PartTimeEmployee : public Employee{
+    protected:
+        int hourlywage;
+        double hoursworked;
+    public:
+        PartTimeEmployee(const string& name_, const int& id_, const int& wage_, const double& hours_): Employee(name_, id_), hourlywage(wage_), hoursworked(hours_){};
+        double calculateSalary() const override;
+        void display() const override;
+};
+class Manager : public FullTimeEmployee, PartTimeEmployee{
+    protected:
+    public:
+        double amount_bonus = PartTimeEmployee::calculateSalary();
+        double base_salary = FullTimeEmployee::calculateSalary();
+        Manager(const string& name_, int id_, double salary_, double wage_, double hours_)
+        : FullTimeEmployee(name_, id_, salary_), PartTimeEmployee(name_, id_, wage_, hours_) {}
+        double calculateSalary() const override;
+        void display() const override;
+};
+#endif
